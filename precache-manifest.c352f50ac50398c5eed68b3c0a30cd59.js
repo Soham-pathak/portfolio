dc4b897775df5d0c8801f4bcc972afd21dff303c
Soham-pathak <soham.pathak@mail.utoronto.ca>
2021-04-23 16:47:57 -0400

    Updates

---
+++ precache-manifest.c352f50ac50398c5eed68b3c0a30cd59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1aac8c3d6b3d9297cf2bf99e205ed3e",
+    "url": "/%7Breact-website%7D/index.html"
+  },
+  {
+    "revision": "7438839cbbf03128d4b7",
+    "url": "/%7Breact-website%7D/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "299a5beece3eeacbc686",
+    "url": "/%7Breact-website%7D/static/css/main.064f7bd3.chunk.css"
+  },
+  {
+    "revision": "7438839cbbf03128d4b7",
+    "url": "/%7Breact-website%7D/static/js/2.92a063fc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/%7Breact-website%7D/static/js/2.92a063fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299a5beece3eeacbc686",
+    "url": "/%7Breact-website%7D/static/js/main.c9f2ef31.chunk.js"
+  },
+  {
+    "revision": "f4a1dd416bd38dea0af2",
+    "url": "/%7Breact-website%7D/static/js/runtime-main.980e9a98.js"
+  }
+]);
