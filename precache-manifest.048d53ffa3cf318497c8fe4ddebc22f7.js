dd598fbc51b2c253c90f21ef284c344b31f95e6a
Soham-pathak <soham.pathak@mail.utoronto.ca>
2021-04-23 17:07:04 -0400

    Updates

---
+++ precache-manifest.048d53ffa3cf318497c8fe4ddebc22f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26077a521f90c12c6340945a6283c2b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7438839cbbf03128d4b7",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8623c69ec8b57136e796",
+    "url": "/portfolio/static/css/main.064f7bd3.chunk.css"
+  },
+  {
+    "revision": "7438839cbbf03128d4b7",
+    "url": "/portfolio/static/js/2.92a063fc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.92a063fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8623c69ec8b57136e796",
+    "url": "/portfolio/static/js/main.527f8998.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%7Bportfolio%7D/precache-manifest.041097f4558b26dbc272c379fbb1248c.js"
+  "/portfolio/precache-manifest.048d53ffa3cf318497c8fe4ddebc22f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%7Bportfolio%7D/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
